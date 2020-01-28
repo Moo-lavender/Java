dad8321df6451846c3df85e06f6d41c79933118c
Moo-laverder <1820545434@qq.com>
2020-01-28 18:06:05 +0800

    最短无序连续子序列-581

---
+++ leetcode练习/FindUnsortedSubarray.java
@@ -0,0 +1,29 @@
+
+public class FindUnsortedSubarray {
+    public static int findUnsortedSubarray(int[] nums) {
+        int len = nums.length;
+        int max = nums[0];
+        int min = nums[len-1];
+        int l = 0, r = -1;
+        for(int i=0;i<len;i++){
+            if(max>nums[i]){
+                r = i;
+            }else{
+                max = nums[i];
+            }
+            if(min<nums[len-i-1]){
+                l = len-i-1;
+            }else{
+                min = nums[len-i-1];
+            }
+        }
+        System.out.println(l);
+        System.out.println(r);
+        return r-l+1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 6, 4, 8, 10, 9, 15};
+        System.out.println(findUnsortedSubarray(nums));
+    }
+}

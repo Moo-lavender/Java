59b30b6cdbcb88b55d0eaab5cf047175887e660b
Moo-laverder <1820545434@qq.com>
2020-02-02 23:13:08 +0800

    二叉树中和为某一值的路径

---
+++ leetcode练习/FindPath.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+//二叉树中和为某一值的路径
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+
+public class FindPath {
+    private ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    private ArrayList<Integer> list = new ArrayList<>();
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
+        if(root == null) return result;
+        list.add(root.val);
+        target -= root.val;
+        if (target == 0 && root.left == null && root.right == null){
+            result.add(new ArrayList<Integer>(list));
+        }
+        FindPath(root.left,target);
+        FindPath(root.right,target);
+        list.remove(list.size()-1);
+        return result;
+    }
+}

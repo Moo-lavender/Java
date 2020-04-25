9a1114a2408dc479d4490b9457cfbe1384964369
Moo-laverder <1820545434@qq.com>
2020-04-25 20:35:46 +0800

    N皇后

---
+++ leetcode练习/回溯算法练习/N皇后.java
@@ -0,0 +1,85 @@
+/*
+n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+
+
+
+上图为 8 皇后问题的一种解法。
+
+给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+
+每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+
+示例:
+
+输入: 4
+输出: [
+ [".Q..",  // 解法 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // 解法 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+解释: 4 皇后问题存在两个不同的解法。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/n-queens
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+class Solution {
+    private static List<String> charToString(char[][] array) {
+        List<String> result = new LinkedList<>();
+        for (char[] chars : array) {
+            result.add(String.valueOf(chars));
+        }
+        return result;
+    }
+    List<List<String>> res ;
+    char[][] board;
+    public List<List<String>> solveNQueens(int n) {
+        if (n <= 0) return null;
+        board = new char[n][n];
+        res = new LinkedList<>();
+        for (char[] chars : board) {
+            Arrays.fill(chars,'.');
+        }
+        back (0);
+        return res;
+    }
+
+    public void back (int row){
+        if (row == board.length) {
+            res.add(charToString(board));
+            return;
+        }
+        int n = board[row].length;
+        for (int col = 0; col < n; col ++) {
+            if (! isValid(row,col)) continue;
+                board[row][col] = 'Q';
+                back(row + 1);
+                board[row][col] = '.';
+        }
+    }
+
+    public boolean isValid(int row,int col) {
+        int n = board.length;
+        for (int i = 0; i < row; i ++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            } 
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+                if (board[i][j] == 'Q') return false;
+            }
+            // check is valide upleft
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+                if (board[i][j] == 'Q') return false;
+            }
+        return true;
+    }
+
+
+}

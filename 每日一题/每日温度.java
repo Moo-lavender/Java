731b7277b7fd2b5c4e188b514702ef4dffc34339
Moo-laverder <1820545434@qq.com>
2020-06-01 19:06:10 +0800

    [739]每日温度

--- 每日一题/每日温度.java
+++ 每日一题/每日温度.java
@@ -9,7 +9,27 @@
 链接：https://leetcode-cn.com/problems/daily-temperatures
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
-public int[] dailyTemperatures(int[] T) {
+public int[] dailyTemperatures1(int[] T) {
+	//使用单调递减栈存储下标
+	//遍历数字，小于栈顶元素入栈，大于栈顶元素不断出栈，计算对应栈顶元素的结果为 i-stack.pop()
+	//遍历结束后还在栈中的元素就是之后都不会升高的元素了，默认为0
+	
+   
+	int[] res = new int[T.length];
+
+	Stack<Integer> stack = new Stack<>();
+	for (int i = 0; i < T.length; i ++) {
+		while (!stack.isEmpty() && T[stack.peek()] < T[i]) {
+			int index = stack.pop();
+			res[index] = i - index;
+		}
+		stack.push(i);
+	}
+	return res;
+}
+public int[] dailyTemperatures2(int[] T) {
+	//从最后一天倒推來，最后一天为0，看倒数第二天的，如果比后一天小，结果为1
+	//否则，如果后一天为0，结果为0，如果都不是，就和比后一天大的那一天进行比较
     int[] res = new int[T.length];
     res[T.length - 1] = 0;
     for (int i = T.length - 2; i >= 0; i--) {

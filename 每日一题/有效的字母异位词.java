a27dc5593062881c0c8062046422080a15998106
Moo-laverder <1820545434@qq.com>
2020-07-19 23:58:57 +0800

    242. 有效的字母异位词

---
+++ 每日一题/有效的字母异位词.java
@@ -0,0 +1,64 @@
+/*
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+示例 1:
+
+输入: s = "anagram", t = "nagaram"
+输出: true
+示例 2:
+
+输入: s = "rat", t = "car"
+输出: false
+说明:
+你可以假设字符串只包含小写字母。
+
+进阶:
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-anagram
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+class Solution {
+    public boolean isAnagram_1(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return String.valueOf(sChars).equals(String.valueOf(tChars));
+    }
+
+    public boolean isAnagram_2(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (char ch : t.toCharArray()) {
+            Integer count = map.get(ch);
+            if (count == null) {
+                return false;
+            } else if (count > 1) {
+                map.put(ch, count - 1);
+            } else {
+                map.remove(ch);
+            }
+        }
+        return map.isEmpty();
+    }
+
+    public boolean isAnagram(String s, String t) {
+        int[] sCounts = new int[26];
+        int[] tCounts = new int[26];
+        for (char ch : s.toCharArray()) {
+            sCounts[ch - 'a']++;
+        }
+        for (char ch : t.toCharArray()) {
+            tCounts[ch - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (sCounts[i] != tCounts[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
